af60b21a1dc574cc6f1d8e49b97cd1e27f0e1928
Francois Melkonian <francois.melkonian@etu.unice.fr>
2017-06-14 08:28:03 +0200

    Suppression des logs de débuggage

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -18,7 +18,9 @@ export class MessageListComponent implements OnInit {
 
   private reload_loop() {
     setInterval(() => {
-      this.messageService.getMessages(this.rte);
+      if (!this.historyAsked) {
+        this.messageService.getMessages(this.rte);
+      }
     }, 1000);
   }
 
@@ -32,15 +34,6 @@ export class MessageListComponent implements OnInit {
     this.messageService.getMessages(this.rte + "?page=" + this.historyLoaded);
   }
 
-  /**
-   * Fonction ngOnInit.
-   * Cette fonction est appelée après l'execution de tous les constructeurs de toutes les classes typescript.
-   * Cette dernière s'avère très utile lorsque l'on souhaite attendre des valeurs venant de d'autres composants.
-   * Le composant MessageComponent prend en @Input un channel. Les @Input ne sont accessibles uniquement à partir du ngOnInit,
-   * pas dans le constructeur.
-   * En general, l'utilisation des services dans le NgOnInit est une bonne practice. Le constructeur ne doit servir qu'à
-   * l'initialisation simple des variables. Pour plus d'information sur le ngOnInit, il y a un lien dans le README.
-   */
   ngOnInit() {
     this.route.subscribe((route) => {
       this.rte = route;
@@ -52,15 +45,13 @@ export class MessageListComponent implements OnInit {
   }
 
   private addNew(messages: MessageModel[]) {
-    if(this.historyAsked) {
+    if (this.historyAsked) {
       this.historyAsked = false;
-      console.log("historique demandé");
       this.addHistory(messages);
     }
     if (this.modified) {
       this.modified = false;
       this.messageList = messages.reverse();
-      console.log("R",messages.length);
       return;
     }
     if ((typeof this.messageList) == "undefined") {
@@ -74,7 +65,6 @@ export class MessageListComponent implements OnInit {
       if (last.id !== messages[i].id) {
         news = news.concat([messages[i]]);
       } else {
-        console.log(news.length,"Ajoutés !");
         this.messageList = this.messageList.concat(news.reverse());
         break;
       }
@@ -85,12 +75,10 @@ export class MessageListComponent implements OnInit {
 
     let first;
     first = this.messageList[0];
-    console.log("Plus ancien actuellement affiché:"+first.content);
     let tab = [];
     for (let i = 0; i < messages.length; i++) {
-      if(first.id != messages[i].id){
-        console.log("ajout",messages[i].content);
-          tab = [messages[i]].concat(tab);
+      if (first.id != messages[i].id) {
+        tab = [messages[i]].concat(tab);
       }
     }
 
