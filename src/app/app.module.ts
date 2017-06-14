482fca7dbf215fe18596ebe7260022a7ed3c7b66
Francois Melkonian <francois.melkonian@etu.unice.fr>
2017-06-14 11:43:50 +0200

    Affichage des vidéos Youtube

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,23 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
-import { RouterModule } from "@angular/router";
-
-import { AppComponent } from "./app.component";
-import { MessageComponent, MessageListComponent } from "./messages";
-import { ChannelComponent} from "./chanels";
-import { MessageFormComponent } from "./message-form";
-import { MessageService } from "../shared/services/message/message.service";
-import { ChannelService } from "../shared/services/channel/channel.service";
-import {UserService } from "../shared/services/user/user.service";
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {RouterModule} from "@angular/router";
+import {AppComponent} from "./app.component";
+import {MessageComponent, MessageListComponent} from "./messages";
+import {ChannelComponent} from "./chanels";
+import {MessageFormComponent} from "./message-form";
+import {MessageService} from "../shared/services/message/message.service";
+import {ChannelService} from "../shared/services/channel/channel.service";
+import {UserService} from "../shared/services/user/user.service";
 import {ChannelListComponent} from "./chanels/channel-list/channel-list.component";
 import {ChannelFormComponent} from "./channel-form/channel-form.component";
 import {ChatComponent} from "./chat/chat.component";
-
 import {PageComponent} from "./chanels/page/page.component";
 import {UserListComponent} from "./users/user-list/user-list.component";
 import {UserComponent} from "./users/user/user.component";
 import {WeatherService} from "../shared/services/weather/weather.service";
+import {SafePipe} from "../shared/pipes/SafePipe";
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,7 +32,8 @@ import {WeatherService} from "../shared/services/weather/weather.service";
     UserListComponent,
     UserComponent,
     ChannelFormComponent,
-    ChatComponent
+    ChatComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
@@ -41,7 +41,7 @@ import {WeatherService} from "../shared/services/weather/weather.service";
     HttpModule,
     RouterModule.forRoot([
       {
-        path: 'threads/:id',
+        path: "threads/:id",
         component: ChatComponent
       }
     ])
